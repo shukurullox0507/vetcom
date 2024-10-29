a958f89872340ee9e0ac661fcf457a27e9aeef4f
asliddinnuriddinov <eshmat010@gmail.com>
2024-10-29 21:32:35 +0500

    created mobile header and added to all pages

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -15,3 +15,18 @@ langBar.addEventListener('click',()=>{
   dropdownBar.classList.toggle('active')
   langBar.classList.toggle('active')
 })
+
+const burger = document.querySelector('#burger')
+const mobileHeader = document.querySelector('.mobile-header')
+const closeButton = document.querySelector('.mobile-header__close-btn');
+let mobileHeaderOpen=false
+
+burger.addEventListener('click', () => {
+  mobileHeaderOpen = !mobileHeaderOpen;
+  mobileHeader.classList.toggle('open', mobileHeaderOpen);
+});
+
+closeButton.addEventListener('click', () => {
+  mobileHeaderOpen = false;
+  mobileHeader.classList.remove('open');
+});
